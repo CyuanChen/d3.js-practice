aa87c07efc2188b7de975080f1e79cf8258b99e6
PeterChen <peterchen302@gmail.com>
2019-02-14 16:39:36 +0800

    [add] Add 5MA 20MA 60MA into candlestick

--- Candlestick/app.js
+++ Candlestick/app.js
@@ -13,7 +13,15 @@ var margin = {top: 20, right: 20, bottom: 30, left: 50},
     var candlestick = techan.plot.candlestick()
             .xScale(x)
             .yScale(y);
-
+    var sma0 = techan.plot.sma()
+            .xScale(x)
+            .yScale(y);
+    var sma1 = techan.plot.sma()
+            .xScale(x)
+            .yScale(y);
+    var ema2 = techan.plot.ema()
+            .xScale(x)
+            .yScale(y);
     var xAxis = d3.axisBottom()
             .scale(x);
 
@@ -49,6 +57,14 @@ var margin = {top: 20, right: 20, bottom: 30, left: 50},
                 .attr("class", "candlestick");
 
         svg.append("g")
+                .attr("class", "sma ma-0");
+        svg.append("g")
+                .attr("class", "sma ma-1");
+        svg.append("g")
+                .attr("class", "ema ma-2");
+        
+        
+        svg.append("g")
                 .attr("class", "x axis")
                 .attr("transform", "translate(0," + height + ")");
 
@@ -72,6 +88,11 @@ var margin = {top: 20, right: 20, bottom: 30, left: 50},
         y.domain(techan.scale.plot.ohlc(data, candlestick.accessor()).domain());
 
         svg.selectAll("g.candlestick").datum(data).call(candlestick);
+        
+        svg.select("g.sma.ma-0").datum(techan.indicator.sma().period(10)(data)).call(sma0);
+        svg.select("g.sma.ma-1").datum(techan.indicator.sma().period(20)(data)).call(sma0);
+        svg.select("g.ema.ma-2").datum(techan.indicator.sma().period(50)(data)).call(sma0);
+        
         svg.selectAll("g.x.axis").call(xAxis.tickFormat(d3.timeFormat("%m/%d")));
         svg.selectAll("g.y.axis").call(yAxis);
     }
