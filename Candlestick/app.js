daeef4e674a0b2d8654480a72d6bdefaf202eb37
PeterChen <peterchen302@gmail.com>
2019-02-14 17:28:41 +0800

    [add] Add Volume in candlestick

--- Candlestick/app.js
+++ Candlestick/app.js
@@ -10,9 +10,13 @@ var margin = {top: 20, right: 20, bottom: 30, left: 50},
     var y = d3.scaleLinear()
             .range([height, 0]);
 
+    var yVolume = d3.scaleLinear()
+            .range([y(0), y(0.2)]);
+
     var candlestick = techan.plot.candlestick()
             .xScale(x)
             .yScale(y);
+
     var sma0 = techan.plot.sma()
             .xScale(x)
             .yScale(y);
@@ -22,11 +26,20 @@ var margin = {top: 20, right: 20, bottom: 30, left: 50},
     var ema2 = techan.plot.ema()
             .xScale(x)
             .yScale(y);
+
+    var volume = techan.plot.volume()
+            .accessor(candlestick.accessor())
+            .xScale(x)
+            .yScale(yVolume);
     var xAxis = d3.axisBottom()
             .scale(x);
 
     var yAxis = d3.axisLeft()
             .scale(y);
+    var volumeAxis = d3.axisRight(yVolume)
+            .ticks(3)
+            .tickFormat(d3.format(",.3s"));
+
 
     var svg = d3.select("body").append("svg")
             .attr("width", width + margin.left + margin.right)
@@ -62,7 +75,10 @@ var margin = {top: 20, right: 20, bottom: 30, left: 50},
                 .attr("class", "sma ma-1");
         svg.append("g")
                 .attr("class", "ema ma-2");
-        
+        svg.append("g")
+                .attr("class", "volume");
+        svg.append("g")
+                .attr("class", "volume axis");
         
         svg.append("g")
                 .attr("class", "x axis")
@@ -86,13 +102,28 @@ var margin = {top: 20, right: 20, bottom: 30, left: 50},
     function draw(data) {
         x.domain(data.map(candlestick.accessor().d));
         y.domain(techan.scale.plot.ohlc(data, candlestick.accessor()).domain());
+        yVolume.domain(techan.scale.plot.volume(data).domain());
 
         svg.selectAll("g.candlestick").datum(data).call(candlestick);
         
         svg.select("g.sma.ma-0").datum(techan.indicator.sma().period(10)(data)).call(sma0);
         svg.select("g.sma.ma-1").datum(techan.indicator.sma().period(20)(data)).call(sma0);
         svg.select("g.ema.ma-2").datum(techan.indicator.sma().period(50)(data)).call(sma0);
+        svg.select("g.volume").datum(data).call(volume);
         
         svg.selectAll("g.x.axis").call(xAxis.tickFormat(d3.timeFormat("%m/%d")));
         svg.selectAll("g.y.axis").call(yAxis);
+        svg.select("g.volume.axis").call(volumeAxis);
     }
+
+
+
+
+
+
+
+
+
+
+
+
